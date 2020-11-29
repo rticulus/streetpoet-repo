517a51a4ccdaf310defd4f9e812a7197d26347d8
RTiculus <samuelwashere2@gmail.com>
2020-11-29 02:40:14 +0000

    Added navbar and beginnings of signup form

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,9 @@ def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
