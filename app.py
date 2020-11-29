a7391d8b83272dc875bc3b0ff86a64be943585ef
RTiculus <samuelwashere2@gmail.com>
2020-11-29 16:25:06 +0000

    Intial add of Sign up form

--- app.py
+++ app.py
@@ -24,11 +24,31 @@ def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
+    if request.method == "POST":
+        # does username exist in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("signup"))
+
+        signup = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(signup)
+
+        # user in session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Sign Up Successful!")
     return render_template("signup.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
